d5552d57b4a7801d13c152d283bce8da54dee741
yaowenxu <yaowen1996@hotmail.com>
2021-01-08 14:53:49 +0800

    添加 windows 平台 start.cmd 脚本
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

---
+++ Auth-BUAA-GW/start.cmd
@@ -0,0 +1,10 @@
+@REM xuyaowen
+@echo off
+echo "Connecting to the network!"
+
+@REM set the username
+@REM set the password
+set username=XXXX
+set password=YYYY
+node .\index.js %username% %password%
+@REM pause
